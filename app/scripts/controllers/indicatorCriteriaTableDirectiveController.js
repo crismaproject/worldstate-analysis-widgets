eb02159892df55599517c24845e9980cf789319e
Daniel Meiers <daniel.meiers@cismet.de>
2014-06-12 13:04:30 +0200

    #1 simple css styling of group rows

--- app/scripts/controllers/indicatorCriteriaTableDirectiveController.js
+++ app/scripts/controllers/indicatorCriteriaTableDirectiveController.js
@@ -1,6 +1,6 @@
 angular.module(
     'eu.crismaproject.worldstateAnalysis.controllers'
-).controller(
+    ).controller(
     'eu.crismaproject.worldstateAnalysis.controllers.IndicatorCriteriaTableDirectiveController',
     [
         '$scope',
@@ -9,20 +9,8 @@ angular.module(
         'ngTableParams',
         function ($scope, $filter, analysisService, NgTableParams) {
             'use strict';
-
-            var dataVector, field, getOrderedProperties, group, i, iccData, j,
-                k_outer, k_inner, keys_outer, keys_inner, prop, val;
-
-            dataVector = analysisService.getWorldstateUtils().stripIccData($scope.worldstates, $scope.forCriteria);
-
-            $scope.rows = [];
-            $scope.columns = [
-                {title: $scope.forCriteria ? 'Criteria' : 'Indicators', field: 'f1', visible: true}
-            ];
-
-            getOrderedProperties = function (obj) {
+            var getOrderedProperties = function (obj) {
                 var p, keys;
-
                 keys = [];
                 for (p in obj) {
                     if (obj.hasOwnProperty(p)) {
@@ -30,69 +18,114 @@ angular.module(
                     }
                 }
                 keys.sort();
-
                 return keys;
-            };
-
-            j = 0;
-            iccData = dataVector[0].data;
-            keys_outer = getOrderedProperties(iccData);
-            for (k_outer = 0; k_outer < keys_outer.length; ++k_outer) {
-                group = iccData[keys_outer[k_outer]];
-                $scope.rows[j++] = {f1: {name: group.displayName, icon: group.iconResource}};
-
-                keys_inner = getOrderedProperties(group);
-                for (k_inner = 0; k_inner < keys_inner.length; ++k_inner) {
-                    prop = keys_inner[k_inner];
-                    if (prop !== 'displayName' && prop !== 'iconResource') {
-                        $scope.rows[j++] = {f1: {name: group[prop].displayName, icon: group[prop].iconResource}};
-                    }
-                }
-            }
-
-            for (i = 0; i < dataVector.length; ++i) {
-                field = 'f' + (i + 2);
-                $scope.columns.push({title: dataVector[i].name, field: field, visible: true});
-
-                iccData = dataVector[i].data;
-                j = 0;
-                keys_outer = getOrderedProperties(iccData);
-                for (k_outer = 0; k_outer < keys_outer.length; ++k_outer) {
-                    group = iccData[keys_outer[k_outer]];
-                    $scope.rows[j++][field] = null;
-
-                    keys_inner = getOrderedProperties(group);
-                    for (k_inner = 0; k_inner < keys_inner.length; ++k_inner) {
-                        prop = keys_inner[k_inner];
-                        if (prop !== 'displayName' && prop !== 'iconResource') {
-                            val = group[prop].value;
-                            if (val % 1 !== 0) {
-                                val = $filter('number')(val, 2);
+            },
+                updateTable = function () {
+                    var field, group, i, iccData, j, k_outer, k_inner, keys_outer, keys_inner, prop, val,
+                        dataVector = analysisService.getWorldstateUtils().stripIccData($scope.worldstates, $scope.forCriteria);
+                    $scope.rows = [];
+                    $scope.columns = [{
+                            title: $scope.forCriteria ? 'Criteria' : 'Indicators',
+                            field: 'f1',
+                            visible: true
+                        }];
+                    j = 0;
+                    iccData = dataVector[0].data;
+                    keys_outer = getOrderedProperties(iccData);
+                    for (k_outer = 0; k_outer < keys_outer.length; ++k_outer) {
+                        group = iccData[keys_outer[k_outer]];
+                        $scope.rows[j++] = {
+                            f1: {
+                                name: group.displayName,
+                                icon: group.iconResource
                             }
-                            $scope.rows[j++][field] = {name: val + ' ' + group[prop].unit};
+                        };
+                        keys_inner = getOrderedProperties(group);
+                        for (k_inner = 0; k_inner < keys_inner.length; ++k_inner) {
+                            prop = keys_inner[k_inner];
+                            if (prop !== 'displayName' && prop !== 'iconResource') {
+                                $scope.rows[j++] = {
+                                    f1: {
+                                        name: group[prop].displayName,
+                                        icon: group[prop].iconResource
+                                    }
+                                };
+                            }
                         }
                     }
-                }
+                    for (i = 0; i < dataVector.length; ++i) {
+                        field = 'f' + (i + 2);
+                        $scope.columns.push({
+                            title: dataVector[i].name,
+                            field: field,
+                            visible: true
+                        });
+                        iccData = dataVector[i].data;
+                        j = 0;
+                        keys_outer = getOrderedProperties(iccData);
+                        for (k_outer = 0; k_outer < keys_outer.length; ++k_outer) {
+                            group = iccData[keys_outer[k_outer]];
+                            $scope.rows[j++][field] = null;
+                            keys_inner = getOrderedProperties(group);
+                            for (k_inner = 0; k_inner < keys_inner.length; ++k_inner) {
+                                prop = keys_inner[k_inner];
+                                if (prop !== 'displayName' && prop !== 'iconResource') {
+                                    val = group[prop].value;
+                                    if (val % 1 !== 0) {
+                                        val = $filter('number')(val, 2);
+                                    }
+                                    $scope.rows[j++][field] = {name: val + ' ' + group[prop].unit};
+                                }
+                            }
+                        }
+                    }
+                    if ($scope.tableParams) {
+                        $scope.tableParams.reload();
+                    } else {
+                        $scope.tableParams = new NgTableParams({
+                            page: 1,
+                            count: $scope.rows.length
+                        }, {
+                            counts: [],
+                            total: 1,
+                            getData: function ($defer, params) {
+                                $defer.resolve($scope.rows.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                            }
+                        });
+                    }
+                };
+                
+            $scope.isGroupRow = function(row){
+                return row.f2 === null;
+            };
+            
+            $scope.getRowClass = function (index) {
+                var row = $scope.rows[index];
+                return $scope.isGroupRow(row) ? 'info' : '';
+            };
+            
+            $scope.getRowStyle= function (index) {
+                var row = $scope.rows[index],
+                    groupRowStyle={
+                        'font-weight':'bold'
+                    };
+                return $scope.isGroupRow(row) ? groupRowStyle : '';
+            };
+            
+            if (angular.isDefined($scope.worldstates)) {
+                updateTable();
             }
 
-            $scope.tableParams = new NgTableParams(
-                {
-                    page: 1,
-                    count: $scope.rows.length
-                },
-                {
-                    counts: [],
-                    total: 1,
-                    getData: function ($defer, params) {
-                        $defer.resolve(
-                            $scope.rows.slice(
-                                (params.page() - 1) * params.count(),
-                                params.page() * params.count()
-                            )
-                        );
-                    }
+            $scope.$watch('worldstates', function (newVal, oldVal) {
+                if (newVal !== oldVal) {
+                    updateTable();
                 }
-            );
+            });
+            $scope.$watch('forCriteria', function (newVal, oldVal) {
+                if (newVal !== oldVal) {
+                    updateTable();
+                }
+            });
         }
     ]
-);
+    );
