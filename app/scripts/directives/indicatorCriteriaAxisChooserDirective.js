7147c288edea09e62de30afbd67951998e9a8aa7
Daniel Meiers <daniel.meiers@cismet.de>
2014-07-14 09:49:44 +0200

    #2 implemented a directive for choosing the x / y axis of the relation analyis graph

---
+++ app/scripts/directives/indicatorCriteriaAxisChooserDirective.js
@@ -0,0 +1,25 @@
+angular.module(
+    'eu.crismaproject.worldstateAnalysis.directives'
+).directive(
+    'indicatorCriteriaAxisChooser',
+    [
+        function () {
+            'use strict';
+
+            var scope;
+
+            scope = {
+                iccObject: '=',
+                isXAxis:'@',
+                selectedAxis: '='
+            };
+
+            return {
+                scope: scope,
+                restrict: 'E',
+                templateUrl: 'templates/indicatorCriteriaAxisChooserTemplate.html',
+                controller: 'eu.crismaproject.worldstateAnalysis.controllers.IndicatorCriteriaAxisChooserDirectiveController'
+            };
+        }
+    ]
+);
