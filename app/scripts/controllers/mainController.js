688871f2f6d55238d3306457b3c1918dbefd97b9
Daniel Meiers <daniel.meiers@cismet.de>
2014-09-10 08:44:58 +0200

    changes in the main controller. closed #27

--- app/scripts/controllers/mainController.js
+++ app/scripts/controllers/mainController.js
@@ -14,7 +14,101 @@ angular.module(
         'localStorageService',
         function ($scope, Nodes, Worldstates, localStorageService) {
             'use strict';
+            
+            var createChartModels, getIndicators;
+            $scope.forCriteriaTable = false;
+            $scope.chartModels = [];
+
+            createChartModels = function () {
+                var j, modelArr;
+                $scope.chartModels = [];
+                if ($scope.worldstates && $scope.worldstates.length > 0) {
+                    for (j = 0; j < $scope.worldstates.length; j++) {
+                        modelArr = [];
+                        if ($scope.worldstates[j]) {
+                            modelArr.push($scope.worldstates[j]);
+                        }
+                        if ($scope.worldstateRef) {
+                            modelArr = modelArr.concat($scope.worldstateRef);
+                        }
+                        $scope.chartModels.push(modelArr);
+                    }
+                }
+            };
+
+            getIndicators = function () {
+                var indicatorGroup, indicatorProp, iccObject, group, j,
+                    indicatorName, indicator, add, forEachFunc;
+                forEachFunc = function (value, index, arr) {
+                    if (indicatorName === value.displayName) {
+                        add = false;
+                    }
+                    if (index === arr.length - 1 && add) {
+                        $scope.indicatorVector.push(indicator);
+                    }
+                };
+
+                if ($scope.worldstates && $scope.worldstates.length > 0) {
+                    for (j = 0; j < $scope.worldstates.length; j++) {
+                        iccObject = Worldstates.utils.stripIccData([$scope.worldstates[j]], false)[0];
+                        for (indicatorGroup in iccObject.data) {
+                            if (iccObject.data.hasOwnProperty(indicatorGroup)) {
+                                group = iccObject.data[indicatorGroup];
+                                for (indicatorProp in group) {
+                                    if (group.hasOwnProperty(indicatorProp)) {
+                                        if (indicatorProp !== 'displayName' && indicatorProp !== 'iconResource') {
+                                            if ($scope.indicatorVector.length > 0) {
+                                                indicatorName = group[indicatorProp].displayName;
+                                                indicator = group[indicatorProp];
+                                                add = true;
+                                                $scope.indicatorVector.forEach(forEachFunc);
+                                            } else {
+                                                $scope.indicatorVector.push(group[indicatorProp]);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            $scope.persistCriteriaFunctions = function () {
+                $scope.showPersistSpinner = true;
+                $scope.showPersistDone = false;
+                $timeout(function () {
+                    localStorageService.add('criteriaFunctionSet', $scope.criteriaFunctionSets);
+                    $scope.showPersistSpinner = false;
+                    $scope.showPersistDone = true;
+                    $timeout(function () {
+                        $scope.showPersistDone = false;
+                    }, 1500);
+                }, 500);
+
+            };
+
+            Worldstates.query({level:5},function (data) {
+                $scope.allWorldstates = data;
+            });
+
+            $scope.$watch('worldstateRef', function (newVal, oldVal) {
+                if (newVal !== oldVal) {
+                    createChartModels();
+                    getIndicators();
+                }
+            });
+            $scope.$watch('worldstates', function (newVal, oldVal) {
+                if (newVal !== oldVal && $scope.worldstates) {
+                    createChartModels();
+                    getIndicators();
+                }
+            }, true);
+
+            $scope.indicatorVector = [];
+            
             $scope.criteriaFunctionSet = localStorageService.get('criteriaFunctionSet') || [];
+            $scope.criteriaFunctionSets = $scope.criteriaFunctionSet;
             $scope.selectedCriteriaFunction = $scope.criteriaFunctionSet[0];
             $scope.$watch('criteriaFunctionSet', function (newVal, oldVal) {
                 if (newVal !== oldVal) {
