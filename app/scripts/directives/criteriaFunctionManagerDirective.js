496e3195cfd829c59deb7d97dced5421e4b7eb22
Daniel Meiers <daniel.meiers@cismet.de>
2014-09-19 10:24:25 +0200

    #37 changed direcive interface
    the criteriaFunction manager now binds to a set of worldstates from
    which it extract the indicators instead of a set of indicators directly.

--- app/scripts/directives/criteriaFunctionManagerDirective.js
+++ app/scripts/directives/criteriaFunctionManagerDirective.js
@@ -8,7 +8,7 @@ angular.module(
 
             var scope;
             scope = {
-                indicators:'=',
+                worldstates:'=',
                 criteriaFunctionSet:'=criteriaFunctions'
             };
 
