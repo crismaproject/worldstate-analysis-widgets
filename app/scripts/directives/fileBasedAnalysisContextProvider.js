2db71acbca1d61421e32e6946cca0015eac7aa00
Daniel Meiers <daniel.meiers@cismet.de>
2014-10-31 09:30:16 +0100

    #56 introduce fileBasedContextProvider directive
    that directive can be used to provide all relevant data for the analysis
    widgets. It is based on files, which means that file choosers can be
    used to provide icc-data, criteriafunctions and decision strategies.

---
+++ app/scripts/directives/fileBasedAnalysisContextProvider.js
@@ -0,0 +1,29 @@
+angular.module(
+    'eu.crismaproject.worldstateAnalysis.directives'
+    ).directive(
+    'fileContextProvider',
+    [
+        function () {
+            'use strict';
+
+            var scope;
+
+            scope = {
+                'worldstates': '=',
+                'selectedWorldstates': '=',
+                'decisionStrategies': '=',
+                'selectedDecisionStrategy': '=',
+                'criteriaFunctions': '=',
+                'selectedCriteriaFunction': '='
+
+            };
+
+            return {
+                scope: scope,
+                restrict: 'E',
+                templateUrl: 'templates/fileContextProviderTemplate.html',
+                controller: 'eu.crismaproject.worldstateAnalysis.controllers.FileContextProviderDirectiveController'
+            };
+        }
+    ]
+    );
